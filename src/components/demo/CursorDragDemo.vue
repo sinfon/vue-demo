c6a30cad142d994ce20e4337440b2397e1159bdf
singoasher <singoasher@163.com>
2018-07-03 16:35:19 +0800

    init cursor drag demo

---
+++ src/components/demo/CursorDragDemo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="cursor-drag-demo">
+    <div class="left-side"></div>
+    <div class="splitter"></div>
+    <div class="right-side"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CursorDragDemo'
+}
+</script>
+
+<style scoped>
+.cursor-drag-demo {
+  width: 100%;
+  height: 100%;
+}
+.left-side {
+  background: red;
+  opacity: .1;
+}
+.splitter {
+  width: 30px;
+}
+.right-side {
+  flex: 1;
+  background: blue;
+  opacity: .1;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import CursorDragDemo from '@/components/demo/CursorDragDemo'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    }, {
+      path: '/demo/cursor-drag-demo',
+      name: 'CursorDragDemo',
+      component: CursorDragDemo
     }
   ]
 })
